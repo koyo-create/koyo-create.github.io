fcc610eb6845180801debdf12a20f7badb0b1403
koyo-create <koyobsk@icloud.com>
2024-07-15 22:38:16 +0900

    メニューアイコンのコンポネート化

---
+++ src/app/compornents/Menu.tsx
@@ -0,0 +1,69 @@
+import React from 'react'
+
+const Menu = () => {
+  return (
+    <>
+    <div className="mb-32 text-center container mx-auto py-20">
+        <a
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+        >
+          <h2 className="mb-3 text-2xl font-semibold">
+            NISA{" "}
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              -&gt;
+            </span>
+          </h2>
+          <p className="text-sm opacity-50">
+            2024年から開始された新NISA
+          </p>
+        </a>
+
+        <a
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2 className="mb-3 text-2xl font-semibold">
+            証券会社{" "}
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              -&gt;
+            </span>
+          </h2>
+          <p className="text-sm opacity-50">
+            証券口座開設をするには
+          </p>
+        </a>
+
+        <a
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+        >
+          <h2 className="mb-3 text-2xl font-semibold">
+            投資信託{" "}
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              -&gt;
+            </span>
+          </h2>
+          <p className="text-sm opacity-50">
+            初心者が取り組むべき投資信託
+          </p>
+        </a>
+
+        <a
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+        >
+          <h2 className="mb-3 text-2xl font-semibold">
+            チックリスト{" "}
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              -&gt;
+            </span>
+          </h2>
+          <p className="text-balance text-sm opacity-50">
+            投資を始めるまでの確認事項
+          </p>
+        </a>
+      </div>
+    </>
+  )
+}
+
+export default Menu
