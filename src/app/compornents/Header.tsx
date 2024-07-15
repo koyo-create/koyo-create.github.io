996ec451369e643ba25a8eb46eb1af950f1920f6
koyo-create <koyobsk@icloud.com>
2024-07-15 22:37:33 +0900

    不要箇所の削除・修正

--- src/app/compornents/Header.tsx
+++ src/app/compornents/Header.tsx
@@ -4,16 +4,16 @@ import React from 'react'
 const Header = () => {
   return (
     <>
-    <header className='p-4'>
-    <nav className='flex justify-between mx-auto container items-center flex-col md:flex-row'>
-    <div className='text-4xl mb-4 md:mb-0'>資産運用について</div>
-    <div className='space-x-12'>
-    <Link href='NISA' className='hover:text-gray-500 transition-all duration-300'>NISA</Link>
-    <Link href='NISA'className='hover:text-gray-500 transition-all duration-300'>証券会社</Link>
-    <Link href='NISA'className='hover:text-gray-500 transition-all duration-300'>投資信託</Link>
+    <header className='container mx-auto'>
+    <div className='flex justify-between mx-auto container items-center flex-col md:flex-row p-5'>
+    <div className='font-medium mb-4 md:mb-0 text-2xl'>資産形成について</div>
+    <nav className='md:ml-auto text-base'>
+    <Link href='NISA' className='hover:text-gray-500 transition-all duration-300 mr-5'>NISA</Link>
+    <Link href='NISA'className='hover:text-gray-500 transition-all duration-300 mr-5'>証券会社</Link>
+    <Link href='NISA'className='hover:text-gray-500 transition-all duration-300 mr-5'>投資信託</Link>
     <Link href='NISA'className='hover:text-gray-500 transition-all duration-300'>チェックリスト</Link>
-    </div>
     </nav>
+    </div>
     </header>
     </>
   )
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
-import Image from "next/image";
 import Header from "./compornents/Header";
+import Menu from "./compornents/Menu";
 
 export default function Home() {
   return (
@@ -7,94 +7,22 @@ export default function Home() {
     <header>
       <Header />
     </header>
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-          </a>
+    {/* <main className="flex min-h-screen flex-col items-center justify-between p-24"> */}
+      {/* <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
+      </div> */}
+
+      <section className="text-gray-700">
+        <div className="flex container mx-auto py-20 px-5 flex-col md:flex-row items-center justify-center">
+        <main className="text-2xl sm:text-6xl font-semibold md:w-1/2 mb-16 lg:pr-24 md:pr-16 text-center md:text-left">
+        これからの
+        <br />
+        資産形成について</main>
+        <div className="md:w-1/2 lg:max-w-lg"><img src="./img/main.jpeg" /></div>
         </div>
-      </div>
-
+      </section>
+    <Menu />
       
-        <main className="mb-3 text-2xl font-semibold">これからの資産形成について</main>
-      
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            NISA{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            2024年から開始された新NISA
-          </p>
-        </a>
-
-        <a
-          href=""
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            証券会社{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            証券口座開設をするには
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            投資信託{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            初心者が取り組むべき投資信託
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            チックリスト{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            投資を始めるまでの確認事項
-          </p>
-        </a>
-      </div>
-    </main>
+    {/* </main> */}
     </>
   );
 }
