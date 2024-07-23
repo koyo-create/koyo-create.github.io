de26695e204fb8407293ed0958565fd3a2ba497e
koyo-create <koyobsk@icloud.com>
2024-07-24 06:29:17 +0900

    Headerの調整

--- src/app/compornents/Header.tsx
+++ src/app/compornents/Header.tsx
@@ -6,12 +6,12 @@ const Header = () => {
     <>
     <header className='container mx-auto'>
     <div className='flex justify-between mx-auto container items-center flex-col md:flex-row p-5'>
-    <div className='font-medium mb-4 md:mb-0 text-2xl'>資産形成について</div>
+    <div className='font-medium mb-4 md:mb-0 text-2xl ml-10'>資産形成について</div>
     <nav className='md:ml-auto text-base'>
-    <Link href='NISA' className='hover:text-gray-500 transition-all duration-300 mr-5'>NISA</Link>
-    <Link href='NISA'className='hover:text-gray-500 transition-all duration-300 mr-5'>証券会社</Link>
-    <Link href='NISA'className='hover:text-gray-500 transition-all duration-300 mr-5'>投資信託</Link>
-    <Link href='NISA'className='hover:text-gray-500 transition-all duration-300'>チェックリスト</Link>
+    <Link href='NISA' className='hover:text-gray-500 transition-all duration-300 mr-10'>NISA</Link>
+    <Link href='SecuritiesCompany'className='hover:text-gray-500 transition-all duration-300 mr-10'>証券会社</Link>
+    <Link href='InvestmentTrust'className='hover:text-gray-500 transition-all duration-300 mr-10'>投資信託</Link>
+    <Link href='Checklist'className='hover:text-gray-500 transition-all duration-300 mr-10'>チェックリスト</Link>
     </nav>
     </div>
     </header>
