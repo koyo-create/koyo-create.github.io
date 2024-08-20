090126e6888882e61ae4a9f444ca4686aab861a9
koyo-create <koyobsk@icloud.com>
2024-08-21 07:26:29 +0900

    確認事項項目の追加

--- src/app/Checklist/page.tsx
+++ src/app/Checklist/page.tsx
@@ -1,8 +1,28 @@
-export default function Checklist() {
+import { AddTask } from "../compornents/AddTask"
+import Header from "../compornents/Header"
+import { TodoList } from "../compornents/TodoList"
+import { getAllTodos } from "../../api";
+
+export default async function Home() {
+  const todos = await getAllTodos();
+  
   return (
     <>
-    <h1>チェックリスト
-    </h1>
+    <Header/>
+    <main className="flex flex-col items-center justify-center min-h-screen py-2 bg-gray-200">
+      <h1 className="text-4xl font-bold text-gray-700 -mt-32">
+        投資を始めるためのSTEP</h1>
+        <p className="text-2xl pt-10 text-center">理解できたら消して
+          <br/>
+          自分のこれからの目標などを入力しよう
+        </p>
+      <div className="w-full max-w-xl mt-5">
+        <div className="w-full px-8 py-6 bg-white shadow-md rounded-lg">
+          <AddTask/>
+          <TodoList todos={todos}/>
+        </div>
+      </div>
+    </main>
     </>
   )
 }
