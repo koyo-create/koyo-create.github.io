b8c5aef69a1c9564fcda12f47f701851a1b33da5
koyo-create <koyobsk@icloud.com>
2024-08-26 07:47:07 +0900

    リンクをバッククウォートに変更

--- src/api.ts
+++ src/api.ts
@@ -1,7 +1,7 @@
 import { Task } from "./types";
 
 export const getAllTodos = async(): Promise<Task[]> => {
-  const res = await fetch('http://localhost:3001/tasks',{
+  const res = await fetch(`http://localhost:3001/tasks`,{
     cache: "no-store", 
 });
 const todos = res.json();
@@ -9,7 +9,7 @@ return todos;
 };
 
 export const addTodo = async(todo:Task): Promise<Task> => {
-  const res = await fetch('http://localhost:3001/tasks',{
+  const res = await fetch(`http://localhost:3001/tasks`,{
     method: "POST",
     headers: {
       "Content-Type": "application/json",
