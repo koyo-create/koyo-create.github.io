88098553a1eedd175b7a8527dbd75b1fb5d991dc
koyo-create <koyobsk@icloud.com>
2024-10-27 12:47:34 +0900

    Homeページの修正

---
+++ src/app/Home/page.jsx
@@ -0,0 +1,55 @@
+'use client'
+import Header from "../compornents/Header";
+import Menu from "../compornents/Menu";
+import {useAuthState} from "react-firebase-hooks/auth"
+import { auth } from "../firebase/firebase";
+import { useRouter } from "next/navigation";
+import { signOut } from "firebase/auth";
+
+export default function Home() {
+  const [user] = useAuthState(auth);
+  const router = useRouter();
+  const userSession = sessionStorage.getItem('user');
+
+console.log({user})
+
+  if (!user && !userSession){
+    router.push('/')
+  }
+
+  return (
+    <>
+    <header>
+      <Header />
+    </header>
+    <section>
+      <div className="container mx-auto flex justify-center">
+    <button className="bg-gray-300 p-4 rounded-lg" onClick={() => {
+      signOut(auth)
+      sessionStorage.removeItem('user')
+    }}>
+                Log Out
+              </button>
+      </div>
+                </section>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24 pt-0">
+      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
+      </div>
+
+      <section className="text-gray-700">
+        <div className="flex container mx-auto py-20 px-5 flex-col md:flex-row items-center justify-center">
+        <main className="text-2xl sm:text-5xl font-semibold md:w-1/2 mb-16 lg:pr-24 md:pr-16 text-center md:text-left">
+        これからの
+        <br />
+        資産形成について</main>
+        <div className="md:w-1/2 lg:max-w-lg"><img src="./img/main.jpeg" /></div>
+        </div>
+        <p className="text-center text-3xl font-bold">今すぐにでも始めるべきNISA制度</p>
+        <p className="text-center text-xl mt-5">初心でも始めやすいNISAを活用した資産形成とは</p>
+      </section>
+    <Menu />
+    </main>
+    </>
+  );
+}
+
