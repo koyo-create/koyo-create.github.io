60ac37edefbe12f24ae6e065e194fa5664754077
koyo-create <koyobsk@icloud.com>
2024-10-27 12:45:44 +0900

    リンクの修正

--- src/app/compornents/Header.tsx
+++ src/app/compornents/Header.tsx
@@ -1,3 +1,4 @@
+
 import Link from "next/link";
 import React from "react";
 
@@ -7,33 +8,34 @@ const Header = () => {
       <header className="container mx-auto">
         <div className="flex justify-between mx-auto container items-center flex-col md:flex-row p-5">
           <div className="font-medium mb-4 md:mb-0 text-2xl ml-10">
-            <Link href="/">資産形成について</Link>
+            <Link href="Home">資産形成について</Link>
           </div>
           <nav className="md:ml-auto text-base">
           <Link
               href="Checklist"
               className="hover:text-gray-500 transition-all duration-300 mr-10"
-            >
+              >
               確認事項
             </Link>
             <Link
               href="NISA"
               className="hover:text-gray-500 transition-all duration-300 mr-10"
-            >
+              >
               NISA
             </Link>
             <Link
               href="InvestmentTrust"
               className="hover:text-gray-500 transition-all duration-300 mr-10"
-            >
+              >
               投資信託
             </Link>
             <Link
               href="SecuritiesCompany"
               className="hover:text-gray-500 transition-all duration-300 mr-10"
-            >
+              >
               証券会社
             </Link>
+              
           </nav>
         </div>
       </header>
