78b08ba0623d1c172fa9feebfe71f87e78cf53c5
koyo-create <koyobsk@icloud.com>
2024-11-25 15:16:57 +0900

    不要箇所の削除

--- src/pages/api/api.ts
+++ src/pages/api/api.ts
@@ -1,8 +1,5 @@
 import { Task } from "../../types";
 
-
-
-
 export const getAllTodos = async(): Promise<Task[]> => {
   const res = await fetch(`http://localhost:3001/tasks`,{
     cache: "no-store", 
