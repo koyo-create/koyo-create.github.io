8bac4c41af85b2ef8dd0fd629522fb1f190b1bcf
koyo-create <koyobsk@icloud.com>
2024-12-11 20:12:02 +0900

    不要コメントの削除

--- src/app/firebase/config.js
+++ src/app/firebase/config.js
@@ -1,20 +1,3 @@
-// import { initializeApp, getApps, getApp } from "firebase/app";
-// import { getAuth } from "firebase/auth";
-
-// const firebaseConfig = {
-//   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-//   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-//   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-//   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-//   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-//   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
-// };
-
-// const app = !getApps().length ?  initializeApp(firebaseConfig) : getApp();
-
-// const auth = getAuth(app);
-
-// export {app, auth};
 import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
@@ -26,6 +9,7 @@ const firebaseConfig = {
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
+
 
 // Firebaseアプリの初期化
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
